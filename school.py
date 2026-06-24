3b710778c8472000dfa8bd73f4e7eddf3a22a381
Waliullah <waliullah.godev@gmail.com>
2026-06-24 21:01:09 +0600

    def grade_to_value(grade):

--- school.py
+++ school.py
@@ -18,21 +18,38 @@ class school:
         pass
     
     
+        
+    @staticmethod
+    def calculate_grade(marks):
+        if marks >= 80 and marks <= 100:
+            return "A+"
+        elif marks >= 70 and marks < 80:
+            return "A"
+        elif marks >= 60 and marks < 70:
+            return "A-"
+        elif marks >= 50 and marks < 60:
+            return "B"
+        elif marks >= 40 and marks < 50:
+            return "C"
+        elif marks >= 33 and marks < 40:
+            return "D"
+        else:
+            return "F"
+        
+        
     
-@staticmethod
-def calculate_grade(marks):
-    if marks >= 80 and marks <= 100:
-        return "A+"
-    elif marks >= 70 and marks < 80:
-        return "A"
-    elif marks >= 60 and marks < 70:
-        return "A-"
-    elif marks >= 50 and marks < 60:
-        return "B"
-    elif marks >= 40 and marks < 50:
-        return "C"
-    elif marks >= 33 and marks < 40:
-        return "D"
-    else:
-        return "F"
+    @staticmethod
+    def grade_to_value(grade):
+        grade_values = {
+            "A+": 4.0,
+            "A": 3.7,
+            "A-": 3.3,
+            "B": 3.0,
+            "C": 2.0,
+            "D": 1.0,
+            "F": 0.0
+        }
+        return grade_values.get(grade, 0.0)     
+    
+    
     
