be865d0f0ba9548b6da07451029b191bf2f8b0e3
Waliullah <waliullah.godev@gmail.com>
2026-06-24 20:58:27 +0600

    calculate_grade func

--- school.py
+++ school.py
@@ -16,3 +16,23 @@ class school:
         
     def student_admission(self, student):
         pass
+    
+    
+    
+@staticmethod
+def calculate_grade(marks):
+    if marks >= 80 and marks <= 100:
+        return "A+"
+    elif marks >= 70 and marks < 80:
+        return "A"
+    elif marks >= 60 and marks < 70:
+        return "A-"
+    elif marks >= 50 and marks < 60:
+        return "B"
+    elif marks >= 40 and marks < 50:
+        return "C"
+    elif marks >= 33 and marks < 40:
+        return "D"
+    else:
+        return "F"
+    
