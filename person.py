6c1078d9dae451cfbd08f3b12c76b2ab6d93d6e4
Waliullah <waliullah.godev@gmail.com>
2026-06-25 11:43:23 +0600

    Teacher class

--- person.py
+++ person.py
@@ -1,4 +1,14 @@
+import random
+
 class Person:
     def __init__(self, name):
         self.name = name
+        
+
+
+class Teacher(Person):
+    def __init__(self, name):
+        super().__init__(name)
+    def evaluate_result(self):
+        return random.randint(1, 100)
         
