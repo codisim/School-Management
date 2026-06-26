85038a2e028842c940ba9a3af865942a4c6e02bd
Waliullah <waliullah.godev@gmail.com>
2026-06-26 11:45:53 +0600

    exam func

--- subject.py
+++ subject.py
@@ -1,6 +1,15 @@
+from person import Teacher
+from school import school
+
 class Subject:
     def __init__(self, name, teacher):
         self.name = name
-        self.teacher = teacher
+        self.teacher = teacher  # teacher ar obj
         self.max_marks = 100
         self.pass_marks = 33
+        
+    def exam(self, students):
+        for student in students:
+            mark = self.teacher.evaluate_result() # 1 - 100
+            student.marks[self.name] = mark
+            student.subject_grades[self.name] = school.calculate_grade(mark)
